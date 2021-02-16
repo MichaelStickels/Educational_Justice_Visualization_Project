b0c1a795936609b921105ddbc5254f60b112a1be
Maddie Wong <maddiewong17@gmail.com>
2021-02-15 20:10:51 -0800

    loaded neighborhood data

---
+++ Summary.R
@@ -0,0 +1,6 @@
+#Compute summary statistics and load stringr and tidyr
+library(stringr)
+library(tidyr)
+
+#Load in data 
+Neighborhoods <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/School%20Neighborhood%20Poverty%20Estimates%20(2015-2016)/EDGE_SIDE1216_PUBSCHS1516.csv")
