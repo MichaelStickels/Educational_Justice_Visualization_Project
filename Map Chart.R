d7f11d59b894eb64ad4be00ec5287aaa16d945e0
Michael Stickels <stickelsmt@gmail.com>
2021-02-18 00:18:43 -0800

    Display CWIFT data on map

--- Map Chart.R
+++ Map Chart.R
@@ -1,34 +1,48 @@
 library(ggplot2)
 library(ggmap)
 library(tidyverse)
+library(mapproj)
+
 
 
 # Load datasets
-census_data <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/Selected%20Economic%20Characteristics%20of%20Relevant%20Children%20Enrolled%20(Public%20and%20Private)%20(2014-2018)/CDP03_104_USSchoolDistrictAll.csv")
+cwift_data <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/Comparable%20Wage%20Index%20for%20Teachers%20(2016)/EDGE_ACS_CWIFT2016_State.csv")
 
 state_codes <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/FIPS_State_Codes.csv")
 
 
 # Manipulate and extract data
-joined_census_data <- census_data %>%
-  select(GeoId, Geography, )
-  mutate(state_code = as.integer(substr(GeoId, 8, 9))) %>%
-  left_join(state_codes, by = "state_code")
+cwift_state <- cwift_data %>%
+  mutate(region = tolower(ST_NAME)) %>%
+  select(region, ST_CWIFTEST)
 
 
-
+# Define a minimalist theme for maps
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),        # remove axis lines
+    axis.text = element_blank(),        # remove axis labels
+    axis.ticks = element_blank(),       # remove axis ticks
+    axis.title = element_blank(),       # remove axis titles
+    plot.background = element_blank(),  # remove gray background
+    panel.grid.major = element_blank(), # remove major grid lines
+    panel.grid.minor = element_blank(), # remove minor grid lines
+    panel.border = element_blank()      # remove border around plot
+  )
 
 
 # Load a shapefile of U.S. states using ggplot's `map_data()` function
-state_shape <- map_data("state")
+state_shape <- map_data("state") %>%
+  left_join(cwift_state, by = "region")
 
 # Create a blank map of U.S. states
-  ggplot(state_shape) +
+ggplot(state_shape) +
   geom_polygon(
-    mapping = aes(x = long, y = lat, group = group),
+    mapping = aes(x = long, y = lat, group = group, fill = ST_CWIFTEST),
     color = "white", # show state outlines
     size = .1        # thinly stroked
   ) +
-  coord_map() # use a map-based coordinate system
-  
-  
+  coord_map() + # use a map-based coordinate system
+  scale_fill_continuous(low = "Dark Grey", high = "Green") +
+  labs(fill = "CWIFT") +
+  blank_theme # variable containing map styles (defined in next code snippet)
