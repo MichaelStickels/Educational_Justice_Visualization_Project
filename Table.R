f3082ca90640bdf7c0300ce0a6046a67aaed5a4e
Maddie Wong <maddiewong17@gmail.com>
2021-02-17 21:32:02 -0800

    calc summary stats

--- Table.R
+++ Table.R
@@ -4,11 +4,14 @@
 # Load in data frames 
 Neighborhoods <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project
   /main/Data/NCES%20Data/School%20Neighborhood%20Poverty%20Estimates%20(2015-2016)/EDGE_SIDE1216_PUBSCHS1516.csv")
+
 Economic <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_
   Visualization_Project/main/Data/NCES%20Data/Selected%20Economic%20Characteristics%20of%20Relevant%20Children%20Enrolled%20
   (Public%20and%20Private)%20(2014-2018)/CDP03_104_USSchoolDistrictAll.csv")
+
 ComparableWageIndex <- read.delim("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_
   Visualization_Project/main/Data/NCES%20Data/Comparable%20Wage%20Index%20for%20Teachers%20(2016)/EDGE_ACS_CWIFT2016_County.txt")
+
 YaleData <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project
   /main/Data/Yale%20Climate%20Opintion%20Data/YCOM_2020_Data.csv")
 
@@ -21,11 +24,23 @@ CWIAgg = aggregate(ComparableWageIndex, by = list(State = ComparableWageIndex$ST
   Average.CWIFT = ComparableWageIndex$CNTY_CWIFTEST),
   FUN = mean) 
 
+# Neighborhoods dataframe grouped by neighborhoods 
+by_neighborhood <- Neighborhoods %>%
+  group_by(NAME)
 
-# Create aggregate table using Neighborhoods data 
+# Create aggregate table using Neighborhoods data and mean IPR
+NeighborhoodAgg = aggregate(Neighborhoods, by = list(School = Neighborhoods$NAME, 
+  Average.Poverty = Neighborhoods$IPR_EST),
+  FUN = mean) 
+
+# Economic dataframe grouped by school districts 
+by_district <- Economic %>%
+  group_by(Geography)
 
 # Create aggregate table using Economic data 
-
+EconAgg = aggregate(Economic, by = list(District = Economic$Geography, 
+  Year = Economic$Year, CDP = Economic$CDP03_1est),
+  FUN = mean)
 
 
 
