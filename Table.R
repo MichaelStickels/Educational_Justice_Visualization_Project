834fcd8ece4a86409d59cff305da40071abbecaa
Maddie Wong <maddiewong17@gmail.com>
2021-02-17 19:59:37 -0800

    started econ table

--- Table.R
+++ Table.R
@@ -13,3 +13,6 @@ YaleData <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educatio
   /main/Data/Yale%20Climate%20Opintion%20Data/YCOM_2020_Data.csv")
 
 # Create aggregate table using group_by() function 
+EconAgg = aggregate(ComparableWageIndex, by = list(State = ComparableWageIndex$ST_NAME, Average.CWIF = ComparableWageIndex$CNTY_CWIFTEST),
+                   FUN = mean)
+
