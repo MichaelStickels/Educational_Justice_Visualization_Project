99c02377c3dbc2a1d7a638d313876bb95d57366d
Maddie Wong <maddiewong17@gmail.com>
2021-02-18 19:59:12 -0800

    added table to index

--- Table.R
+++ Table.R
@@ -2,15 +2,11 @@
 # in specific neighborhoods, school districts, and states. 
 
 # Load in data frames 
-Neighborhoods <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_
-  Visualization_Project/main/Data/NCES%20Data/School%20Neighborhood%20Poverty%20Estimates%20(2015-2016)/EDGE_SIDE1216_PUBSCHS1516.csv")
+Neighborhoods <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/School%20Neighborhood%20Poverty%20Estimates%20(2015-2016)/EDGE_SIDE1216_PUBSCHS1516.csv")
 
-Economic <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_
-  Visualization_Project/main/Data/NCES%20Data/Selected%20Economic%20Characteristics%20of%20Relevant%20Children%20Enrolled%20
-  (Public%20and%20Private)%20(2014-2018)/CDP03_104_USSchoolDistrictAll.csv")
+Economic <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/Selected%20Economic%20Characteristics%20of%20Relevant%20Children%20Enrolled%20(Public%20and%20Private)%20(2014-2018)/CDP03_104_USSchoolDistrictAll.csv")
 
-ComparableWageIndex <- read.delim("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_
-  Visualization_Project/main/Data/NCES%20Data/Comparable%20Wage%20Index%20for%20Teachers%20(2016)/EDGE_ACS_CWIFT2016_County.txt")
+ComparableWageIndex <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/Comparable%20Wage%20Index%20for%20Teachers%20(2016)/EDGE_ACS_CWIFT2016_County.csv")
 
 YaleData <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project
   /main/Data/Yale%20Climate%20Opintion%20Data/YCOM_2020_Data.csv")
