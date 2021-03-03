b51364b30e0dd772150ade1c2c3027a5c58d818d
Maddie Wong <maddiewong17@gmail.com>
2021-03-02 17:57:27 -0800

    fixed table

--- SummaryTable.R
+++ SummaryTable.R
@@ -11,69 +11,33 @@ Economic <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educatio
 
 ComparableWageIndex <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/NCES%20Data/Comparable%20Wage%20Index%20for%20Teachers%20(2016)/EDGE_ACS_CWIFT2016_County.csv")
 
-YaleData <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project
-  /main/Data/Yale%20Climate%20Opintion%20Data/YCOM_2020_Data.csv")
+YaleData <- read.csv("https://raw.githubusercontent.com/MichaelStickels/Educational_Justice_Visualization_Project/main/Data/Yale%20Climate%20Opintion%20Data/YCOM_2020_Data.csv")
 
 # Aggregated table of Yale Climate Data and create a table by state with percentage of citizens that want to teach 
 # students about global warming and support limiting CO2 emissions. 
-by_location <- YaleData %>%
+education_table <- YaleData %>%
   filter(GeoType == "State") %>% 
   group_by(GeoName) %>%
   filter(teachGW == max(teachGW)) %>%
   filter(CO2limits == max(CO2limits)) %>%
-  select(GeoName, CO2limits, teachGW)
+  select(State = GeoName, Support_Controlling_CO2_Emissions = CO2limits, 
+         Support_Teaching_about_Global_Warming = teachGW)
   
   
-# Create a table using Yale Climate Opinion dataframe 
-Climate_Education <- YaleData %>%
-  select(
-    Location_Type = GeoType, 
-    Location = GeoName, 
-    Population = TotalPop, 
-    Support_Tax_Reduction = reducetax, 
-    Reduce_CO2_limits = CO2limits,
-    Support_Local_Officials = localofficials, 
-    Support_Governor = governor, 
-    Support_Congress = congress, 
-    Support_President = president,
-    Support_Corporations = corporations, 
-    Support_regulating_emissions = regulate, 
-    Support_Offshore_Drilling = drilloffshore, 
-    Support_Global_warming_edu = teachGW,
-    Worried_for_Future = worried
-  ) %>%
-  arrange(Location) %>%
-  top_n(10)
+# Create a table averaging the Comparable Wage Index for Teachers by state 
+cwift_by_state <- ComparableWageIndex %>%
+  group_by(ST_NAME) %>%
+  mutate(CNTY_AVG = mean(CNTY_CWIFTSE)) %>%
+  select(State = ST_NAME, Average_CWIFT = CNTY_AVG) %>%
+  distinct(State, Average_CWIFT)
+  
+  
+# Final table showing how the CWIFT compares to amount of environmental education in each state 
+education_cwift_table <- left_join(education_table, cwift_by_state, by = "State")
 
 
-# Group CWI dataframe by state 
-CWI_by_state <- ComparableWageIndex %>%
-  group_by(ST_NAME)
 
-# Create aggregate table using Comparable Wage Index data 
-CWIAgg = aggregate(ComparableWageIndex, by = list(State = ComparableWageIndex$ST_NAME, 
-  Average.CWIFT = ComparableWageIndex$CNTY_CWIFTEST),
-  FUN = mean) 
 
-# Neighborhoods dataframe grouped by neighborhoods 
-by_neighborhood <- Neighborhoods %>%
-  group_by(NAME)
-
-# Create aggregate table using Neighborhoods data and mean IPR
-NeighborhoodAgg = aggregate(Neighborhoods, by = list(School = Neighborhoods$NAME, 
-  Average.Poverty = Neighborhoods$IPR_EST),
-  FUN = mean) 
-
-# Economic dataframe grouped by school districts 
-by_district <- Economic %>%
-  group_by(Geography)
-
-# Create aggregate table using Economic data 
-EconAgg = aggregate(Economic, by = list(District = Economic$Geography, 
-  Year = Economic$Year, CDP = Economic$CDP03_1moe),
-  FUN = mean)
-
-EconAgg = head(EconAgg)
 
 
 
