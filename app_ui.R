29ce24dae9ba4751c41f1d78699e6f6b81af363f
Katie Jerauld <jerauldk@uw.edu>
2021-03-11 21:57:47 -0800

    Write intro, and update chart text

--- app_ui.R
+++ app_ui.R
@@ -44,12 +44,31 @@ main_page <- tabPanel(
   
   br(),
   
-  p("In this report, we want to answer:", style = "font-size:25px;"),
+  h1("Introduction to Educational Justice"),
+  
+  p("This project studies how inequality affects primary and secondary education
+    and people's environmental beliefs. We primarily used data from the National
+    Center for Education Statistics (NCES) and Yale Climate Opinion. We wanted
+    to investigate these data types together to understand how the United
+    States' education system may be affecting climate change opinions. Climate
+    change is an environmental justice problem because low-income and minority
+    communities will be most vulnerable to its effects. To ensure communities
+    are prepared for the oncoming effects of climate change we must further
+    analyze what is affecting public opinion on the problem, which is why we
+    chose to investigate education data. Through our analysis we hope to further
+    our understanding of the complexities behind climate opinions and prompt
+    further discussion on changing environmental education.", 
+    style = "font-size:25px;"),
+  
+  br(),
+  
+  p("In this report, we want to answer the following questions:", 
+    style = "font-size:25px;"),
   
   h2(
     "How do environmental attitudes indicate the effects of environmental
       education?",
-    style = "font-size:30px;"
+    style = "font-size:25px;"
   ),
   
   p("Comparing data from Yale Climate Opinion Maps", style = "font-size:20px;"),
@@ -58,7 +77,7 @@ main_page <- tabPanel(
   
   h3(
     "How do poverty levels impact attitudes toward environmental education and regulations?",
-    style = "font-size:30px;"
+    style = "font-size:25px;"
   ),
   
   p(
@@ -70,7 +89,7 @@ main_page <- tabPanel(
   br(),
   
   h4("How does school funding impact environmental education?",
-     style = "font-size:30px;"),
+     style = "font-size:25px;"),
   
   p(
     "Comparing data from Comparable Wage Index for Teachers (CWIFT) and Yale
@@ -97,7 +116,15 @@ page_one <- tabPanel(
   p(
     "In this chart we compare the support for climate education in schools to
      other environmental policies to visualize the priority of climate education
-     within American public opinion on climate policy.",
+     within American public opinion on climate policy. When you look at the
+    chart, the state at the top provides the least funding per student and the
+    following states are ordered based on this, so the last state provides the
+    most funding per student. You may select any of the individual policies to
+    compare how public opinion on that policy compares to public opinion on
+    teaching about global warming (climate change) in schools. You may also
+    choose between viewing the trend lines and the mean lines for each policy
+    based on the collective states. Finally, you may select specific states to
+    get a more focused view of select state opinions on these policies.",
     style = "font-size:20px;"
   ),
   
@@ -199,12 +226,32 @@ page_two <- tabPanel(
                 )),
   
   p(
-    "In these charts, you can compare how a state's Income-to-Poverty Ratio (IPR) relates to certain attitudes
-   toward environmental education and/or environmental issues. We wanted to see the correlation between
-   attitudes toward environmental education and the poverty rates in each state. As you can see, depending
-   on the topic/issue, attitudes are often reflective of the IPR in each state as they tend to stay similar
-   when plotting different issues."
-  )
+    "In these charts, you can compare how a state's Income-to-Poverty Ratio
+    (IPR) relates to certain attitudes toward environmental education and/or
+    environmental issues. We wanted to see the correlation between attitudes
+    toward environmental education and the poverty rates in each state. As you
+    can see, depending on the topic/issue, attitudes are often reflective of the
+    IPR in each state as they tend to stay similar when plotting different
+    issues."
+  ),
+  
+  p("These two maps allow for a comparison between poverty data and public
+   support for various aspects of environmental education. The top chart,
+   displays the average income-to-poverty ratio by state. The income-to-poverty
+   ratio is defined as the percentage of family income that is above or below
+   the federal poverty level. It ranges from 0 to 999 where lower IPR values
+   indicate a higher level of poverty. This chart averages the IPR of each state
+   in general, and an IPR value of 100 would represent a family with an income
+   at the poverty threshold. Lighter shades on this chart would therefore
+   indicate that state to be closer to poverty level."),
+  
+  p("The map below the first, will change based on the aspect of environmental
+  education you select to view. The dark shades indicate a lighter the shade,
+  the lower the percentage of support for that policy will be. The purpose of
+  placing these two maps next to each is so the viewer can observe if there is a
+  correlation between the IPR of the state and public opinion on environmental
+  education in that state."),
+  
   
 )
 
